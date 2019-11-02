e1d7c293e527f6a0a4bde36106954f1906b9bc5e
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2019-11-02 14:30:04 +0000

    full CRUD for Customer

--- models/customer.rb
+++ models/customer.rb
@@ -1,39 +1,56 @@
-require 'pg'
 require_relative('../db/sql_runner.rb')
 
 class Customer
 
-  attr_reader :name, :funds, :id
+  attr_accessor :name, :funds
+  attr_reader :id
 
   def initialize(options)
+    @id = options['id'].to_i if options['id']
     @name = options['name']
     @funds = options['funds']
-    @id = options['id'].to_i if options['id']
   end
 
-  def save()
+  def save
     sql = "
     INSERT INTO customers
     (name, funds)
     VALUES
     ($1, $2)
-    RETURNING id"
+    RETURNING id;"
     values = [@name, @funds]
     result = SqlRunner.run(sql, values)
-    @id = result[0]
+    @id = result[0]['id'].to_i
   end
 
-  def update()
+  def update
+    sql = "
+    UPDATE customers SET
+    (name, funds)
+    =
+    ($1, $2)
+    WHERE id = $3;"
+    values = [@name, @funds, @id]
+    SqlRunner.run(sql, values)
   end
 
   def delete()
+    sql = "
+    DELETE FROM customers
+    where id = $1;"
+    values = [@id]
+    SqlRunner.run(sql, values)
   end
 
-  def self.all()
+  def self.all
+    sql = "SELECT * FROM customers;"
+    result = SqlRunner.run(sql)
+    return result.map { |customer| Customer.new(customer) }
   end
 
-  def self.delete_all()
+  def self.delete_all
+    sql = "DELETE FROM stars;"
+    SqlRunner.run(sql)
   end
-
 
 end
